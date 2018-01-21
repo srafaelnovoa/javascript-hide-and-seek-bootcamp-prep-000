a68221d53d1c9df308621aee15250a92117ecdbf
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-21 02:48:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function deepestChild() {
   var deepestNode = current[0]
   var children, currentNode
   console.log(`deepestNode : ${deepestNode }`)
-  while (current && iLoop <100 && current.length > 0) 
+  while (current && iLoop <100 && current !== undefined ) 
   {
     console.log(`iLoop : ${iLoop }, iLevelCurrent : ${iLevelCurrent }, iLevelMax : ${iLevelMax }, current: ${current}, next: ${next}`)
     iLoop++
