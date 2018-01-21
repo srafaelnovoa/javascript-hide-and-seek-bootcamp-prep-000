c75771d9a6be5ffba0332106421322351a1f7d31
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-21 15:35:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,36 +23,4 @@ function deepestChild2() {
   }
     return lis[lis.length -1]
 }
-function deepestChild() {
-  
-  let current = [];
-  let next = [];
-  current.push( document.querySelector('#grand-node') )
-  var iLoop = 0  
-  var deepestNode = current[0]
-  var children, currentNode
-  console.log(`deepestNode : ${deepestNode }`)
-  while (current && iLoop <100 && current[0] ) 
-  {
-    console.log(`iLoop : ${iLoop }, current: ${current}, next: ${next}`)
-    iLoop++
-    currentNode = current.shift()
-    //console.log(`currentNode : ${currentNode},`)
-    children = currentNode.childNodes
-    //console.log(`children : ${children},`)
-    for (let i = 0; i < children.length; i++){
-      
-      if(children[i].nodeName ==="DIV"){
 
-      //console.log(`children[${i}].nodeName : ${children[i].nodeName },`)
-      next.push(children[i])
-      deepestNode = children[i]
-      }
-      
-    }
-    current = [];
-    current.push(next.shift())
-    console.log(" ")
-  }
-    return deepestNode
-}
